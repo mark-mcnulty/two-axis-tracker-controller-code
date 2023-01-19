402357b2c0431d13a0e30ad41e61106d810bb2a9
Zach Steenson <zsteenson@mines.edu>
2023-01-19 08:32:58 -0700

    updated to use Pyqt5

--- Screen01.qml
+++ Screen01.qml
@@ -1,6 +1,5 @@
-import QtQuick
-import QtQuick.Window
-import QtQuick.Controls
+import QtQuick 2.15
+import QtQuick.Controls 2.15
 
 Rectangle {
     id: application_window
--- gui_wrapper.py
+++ gui_wrapper.py
@@ -1,11 +1,11 @@
 import sys
-from PySide6.QtWidgets import QApplication
-from PySide6.QtQuick import QQuickView
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlApplicationEngine
 
-if __name__ == "__main__":
-    app = QApplication()
-    view = QQuickView()
+app = QGuiApplication(sys.argv)
 
-    view.setSource("Screen01.qml")
-    view.show()
-    sys.exit(app.exec())
+engine = QQmlApplicationEngine()
+engine.quit.connect(app.quit)
+engine.load('Screen01.qml')
+
+sys.exit(app.exec())
